0534a1f1d6a368bcd7dce76ff2a1869677f0a526
emel-k <e41@hotmail.fr>
2025-01-31 16:50:02 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function MyForm () {
+    const [text,setText] = useState("")
+    console.log(text)
+    const [todo,setTodo] = useState([])
+
+    function add(){
+        setTodo([...todo,text])
+        setText('')
+        document.querySelector(".searchbar").value = ""
+    }
+
+    return (
+        <div>
+            <label>
+                <input className="searchbar" placeholder="Tapez ce que vous voulez faire dans la journée"
+                       onChange={e => setText(e.target.value)} />
+            </label>
+
+            <button className="btn-search" onClick={add}>
+                Ceci est un bouton
+            </button>
+
+            <ul>
+                {todo.map((item, id) => (
+                    <li key={id}>
+                        {item}
+                        <button className="delete" >supprimer</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <h1>
-
+            Ma To Do list Moche
             </h1>
+            <div className="align_thing">
+                <MyForm />
+
+            </div>
+
         </div>
     )
 }
